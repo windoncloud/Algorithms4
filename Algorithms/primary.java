e371f8c4797b33f01574395445314ea8f446e10b
Kingsley <448409934@qq.com>
2019-12-16 01:28:10 +0800

    gitignore initial twice

---
+++ Algorithms/primary.java
@@ -0,0 +1,12 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("say yes!");
+        int a = First.sa();
+        System.out.print('a' + a);
+    }
+    
+
+    static int sa() {
+        return 1;
+    }
+}
