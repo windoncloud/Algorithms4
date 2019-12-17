c5283dcb1470659cf519f8eba7026b004a373d90
Kingsley <448409934@qq.com>
2019-12-18 00:25:43 +0800

    max profit

--- Algorithms/src/com/company/Main.java
+++ Algorithms/src/com/company/Main.java
@@ -3,8 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int [] arr = new int[] {7, 1, 5, 3, 6, 4};
         int result = gcd(10, 20);
+        int profit = maxProfit(arr);
         System.out.print("result " + result);
+        System.out.print("profit " + profit);
     }
 
     // 计算两个非负整数p和q的最大公约数：若q是0，
@@ -17,5 +20,19 @@ public class Main {
         int r = p % q;
         return gcd(q, r);
     }
+
+    public static int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+    // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode/
 
 }
